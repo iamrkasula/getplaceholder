56fe92af4556e1b9790d181631cd1c63c2135d6f
iamrkasula <iamrkasula@gmail.com>
2019-04-05 04:21:47 +0530

    - cleanup - added home page

--- app.js
+++ app.js
@@ -1,56 +1,43 @@
-var createError = require("http-errors");
-var compression = require("compression");
-var express = require("express");
-var path = require("path");
-var logger = require("morgan");
-var cors = require("cors");
+const compression = require("compression");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
 const validator = require("express-validator");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 
+const app = express();
+
 const limiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minute
-	max: 500 // limit each IP to 200 requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 500 // limit each IP to 200 requests per windowMs
 });
 
-// api version: v1
+// API routes
 var usersRouter = require("./src/v1/routes/users");
 
-var app = express();
-
-// view engine setup
-// app.set("views", path.join(__dirname + "/src", "views"));
-// app.set("view engine", "jade");
-
+// Middleware
+app.use(cors());
 app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 app.use(validator());
 app.use(compression());
 app.use(limiter);
 
+// Endpoints
 app.use("/api/v1/users", usersRouter);
 
-app.get("*", function(req, res, next) {
-	let err = new Error("Page Not Found");
-	err.statusCode = 404;
-	next(err);
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
-});
-
-// error handler
+// Handle Errors
 app.use(function(err, req, res, next) {
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "production" ? err : {};
-	res.status(err.status || 500);
-	res.status(err.statusCode).send(err.message);
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
